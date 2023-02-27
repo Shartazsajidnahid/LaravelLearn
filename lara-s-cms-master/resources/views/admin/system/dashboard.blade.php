96c8735067a12b41a6c604f7402a4afb94725602
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2023-02-27 17:28:02 +0600

    Add files via upload

--- lara-s-cms-master/resources/views/admin/system/dashboard.blade.php
+++ lara-s-cms-master/resources/views/admin/system/dashboard.blade.php
@@ -6,7 +6,8 @@
   <div class="row">
     <!-- message info -->
     @include('_template_adm.message')
-    
+
     <h2 class="text-center">{{ strtoupper(lang('welcome, #name', $translation, ['#name' => Session::get('admin')->name])) }}</h2>
   </div>
 @endsection
+
