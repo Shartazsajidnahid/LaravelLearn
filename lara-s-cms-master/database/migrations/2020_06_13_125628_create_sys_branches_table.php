14a31d94ffcb3475190f7d301943585c0bb929e2
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2023-03-14 17:22:22 +0600

    Add files via upload

--- lara-s-cms-master/database/migrations/2020_06_13_125628_create_sys_branches_table.php
+++ lara-s-cms-master/database/migrations/2020_06_13_125628_create_sys_branches_table.php
@@ -20,14 +20,11 @@ class CreateSysBranchesTable extends Migration
             $table->string('location')->nullable();
             $table->string('gmaps')->nullable();
             $table->string('phone')->nullable();
-            $table->string('cbs_branch_code')->nullable();
             $table->boolean('status')->default(1);
             $table->unsignedInteger('ordinal')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
-
-
 
         $seeder = new SysBranchSeederInitial();
         $seeder->run();
