dcc2306052b5c4d7166d05f2c8f2a47d3a9e246b
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2023-04-06 15:38:52 +0600

    Add files via upload

--- lara-s-cms-master/database/migrations/2023_04_02_082457_create_applinks_table.php
+++ lara-s-cms-master/database/migrations/2023_04_02_082457_create_applinks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTopicsTable extends Migration
+class CreateApplinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,13 @@ class CreateTopicsTable extends Migration
      */
     public function up()
     {
-        Schema::create('topics', function (Blueprint $table) {
+        Schema::create('applinks', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->text('description');
-            $table->boolean('status')->default('1')->comment('0:inactive | 1:active');
+            $table->string('name');
+            $table->string('link');
+            $table->string('image')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
-
-        $seeder = new TopicsSeederInitial();
-        $seeder->run();
     }
 
     /**
@@ -33,6 +29,6 @@ class CreateTopicsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('topics');
+        Schema::dropIfExists('applinks');
     }
 }
--- lara-s-cms-master/database/migrations/2023_04_03_051214_create_top__branches_table.php
+++ lara-s-cms-master/database/migrations/2023_04_03_051214_create_top__branches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTopicsTable extends Migration
+class CreateTopBranchesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,12 @@ class CreateTopicsTable extends Migration
      */
     public function up()
     {
-        Schema::create('topics', function (Blueprint $table) {
+        Schema::create('top__branches', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->text('description');
-            $table->boolean('status')->default('1')->comment('0:inactive | 1:active');
+            $table->integer('rank');
+            $table->bigInteger('branch_id')->index('FK_sys_branches');
             $table->timestamps();
-            $table->softDeletes();
         });
-
-        $seeder = new TopicsSeederInitial();
-        $seeder->run();
     }
 
     /**
@@ -33,6 +28,6 @@ class CreateTopicsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('topics');
+        Schema::dropIfExists('top__branches');
     }
 }
