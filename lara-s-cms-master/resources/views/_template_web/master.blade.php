943e142a722ba035b75a7763f153429d40cb89c9
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2023-05-30 14:05:40 +0600

    Add files via upload

--- lara-s-cms-master/resources/views/_template_web/master.blade.php
+++ lara-s-cms-master/resources/views/_template_web/master.blade.php
@@ -17,6 +17,8 @@
     <!-- Custom styles for this template -->
     <link href="{{ asset('web/css/modern-business.css') }}" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     @yield('css')
 
     @yield('script-head')
@@ -25,7 +27,7 @@
 <body>
 
     <!-- Navigation -->
-    @include('_template_web.nav')
+    {{-- @include('_template_web.nav') --}}
 
     <!-- Page Content -->
     @yield('content')
